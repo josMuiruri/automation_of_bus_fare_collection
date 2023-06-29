ac1e67afa087505d11d633352f8ba6b9ca1a2936
josmuiruri <josemuiruri51@gmail.com>
2023-06-29 19:10:22 +0100

    1

---
+++ automation_of_bus_fare_collection/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for automation_of_bus_fare_collection project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automation_of_bus_fare_collection.settings')
+
+application = get_asgi_application()
